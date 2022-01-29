ecc2a292c68f9196530a9bf2a313ac05b8d84a78
rahulsharma416 <rahul.sharma416@gmail.com>
2022-01-29 22:32:12 +0530

    Remove creds

--- config.php
+++ config.php
@@ -4,35 +4,6 @@ require_once __DIR__ . '/KeycloakWrapper.php';
 
 ini_set('display_errors', 1);
 
-// $config = Keycloak\Admin\KeycloakClient::factory([
-//   'realm' => 'master',
-//   'grant_type' => 'client_credentials',
-//   'username' => 'marc-david.jung@corgos.de',
-//   'password' => 'Winter2021',
-//   'client_id' => 'master-realm',
-//   'client_secret' => 'e6eb8a08-69a9-4419-87f6-e574790db53e',
-//   'baseUri' => 'https://authdev.corgos.de',
-//   'custom_operations' => [
-//     'revokeSessionsForUser' => [
-//         'uri' => 'auth/admin/realms/{realm}/users/{id}/logout',
-//         'description' => 'Logout all the sessions for a specific user',
-//         'httpMethod' => 'POST',
-//         'parameters' => [
-//             'realm' => [
-//                 'location' => 'uri',
-//                 'description' => 'The Realm name',
-//                 'type' => 'string',
-//                 'required' => true,
-//             ],
-//             'id' => [
-//                 'location' => 'uri',
-//                 'type' => 'string',
-//                 'required' => true,
-//             ],
-//         ],
-//     ],
-//   ]
-// ]);
 $config = Keycloak\Admin\KeycloakClient::factory([
   'realm' => 'master',
   'grant_type' => 'client_credentials',
